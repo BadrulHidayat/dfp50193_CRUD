203a535ae789436ae49e24cd3d6a53823a49fe51
BadrulHidayat <badrulhidayat95@gmail.com>
2021-11-15 11:51:16 +0800

    sql

--- db_pelajar.sql
+++ db_pelajar.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 15, 2021 at 04:34 AM
+-- Generation Time: Nov 15, 2021 at 04:50 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 7.4.24
 
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `data_pelajar` (
-  `id` int(11) NOT NULL,
+  `idpelajar` int(11) NOT NULL,
   `nama_pelajar` varchar(200) NOT NULL,
   `noIC_pelajar` varchar(12) NOT NULL,
   `noMatrik_pelajar` varchar(12) NOT NULL,
@@ -39,8 +39,8 @@ CREATE TABLE `data_pelajar` (
 -- Dumping data for table `data_pelajar`
 --
 
-INSERT INTO `data_pelajar` (`id`, `nama_pelajar`, `noIC_pelajar`, `noMatrik_pelajar`, `kelas`) VALUES
-(0, 'Badrul Hidayat', '011016090025', '18DDT19F1005', 'DDT5B / JTMK');
+INSERT INTO `data_pelajar` (`idpelajar`, `nama_pelajar`, `noIC_pelajar`, `noMatrik_pelajar`, `kelas`) VALUES
+(1, 'Badrul Hidayat', '011016090025', '18DDT19F1005', 'DDT5B / JTMK');
 
 --
 -- Indexes for dumped tables
@@ -50,11 +50,21 @@ INSERT INTO `data_pelajar` (`id`, `nama_pelajar`, `noIC_pelajar`, `noMatrik_pela
 -- Indexes for table `data_pelajar`
 --
 ALTER TABLE `data_pelajar`
-  ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`idpelajar`),
   ADD UNIQUE KEY `noIC_pelajar` (`noIC_pelajar`),
   ADD UNIQUE KEY `noPendaftaran_pelajar` (`noMatrik_pelajar`),
   ADD UNIQUE KEY `noMatrik_pelajar` (`noMatrik_pelajar`),
   ADD KEY `kelas` (`kelas`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `data_pelajar`
+--
+ALTER TABLE `data_pelajar`
+  MODIFY `idpelajar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
